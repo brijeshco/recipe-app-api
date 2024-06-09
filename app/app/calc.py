eb800d955cc231038e9bf2350bc195b40cc18b04
Brijesh <forndline@protonmail.com>
2024-06-09 21:22:56 +0530

    fix error postgre

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,9 +2,13 @@
 Calculator functions
 """
 
+
 def add(x,y):
   """Add x and y and return result."""
   return x+y
 
-def subtract(x,y):
-  return y-x
+
+def subtract(x ,y):
+  """Subtract x from y and return result."""
+  return y - x
+
--- app/app/tests.py
+++ app/app/tests.py
@@ -1,5 +1,5 @@
 """
-Sample test
+Sample tests
 """
 
 from django.test import SimpleTestCase
@@ -7,13 +7,16 @@ from django.test import SimpleTestCase
 from app import calc
 
 class CalcTests(SimpleTestCase):
+  """Test the calc module."""
 
   def test_add_numbers(self):
-    res = calc.add(5,6)
+    """Test adding numbers together."""
+    res = calc.add(5, 6)
 
-    self.assertEqual(res,11)
+    self.assertEqual(res, 11)
 
   def test_subtract_numbers(self):
-    res = calc.subtract(10,15)
+    """Test subtracting numbers."""
+    res = calc.subtract(10, 15)
 
-    self.assertEqual(res,5)
+    self.assertEqual(res, 5)
